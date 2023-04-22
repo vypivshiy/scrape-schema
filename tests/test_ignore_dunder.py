b078b8a014b870ef039a170cb2540ea9c03bc9c8
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-04-22 12:16:32 +0300

    update unit test

--- tests/test_ignore_dunder.py
+++ tests/test_ignore_dunder.py
@@ -3,19 +3,30 @@ from typing import Annotated
 from tests.fixtures import MockField
 
 from scrape_schema import BaseSchema
+from scrape_schema.fields import Nested
 
 
 class IgnoreDunderSchema(BaseSchema):
+    class _HiddenSubSchema(BaseSchema):
+        spam: Annotated[str, MockField("egg")]
+
+    class PublicSubSchema(BaseSchema):
+        egg: Annotated[str, MockField("spam")]
+
     _a: Annotated[str, MockField("hidden value")]
     _b: Annotated[int, MockField("100")]
     val: Annotated[str, MockField("visible value")]
+    val_2: Annotated[PublicSubSchema, Nested(PublicSubSchema, crop_rule=lambda _:_)]
+    val_3: Annotated[_HiddenSubSchema, Nested(_HiddenSubSchema, crop_rule=lambda _:_)]
+    _val_4: Annotated[PublicSubSchema, Nested(PublicSubSchema, crop_rule=lambda _:_)]
+    _val_5: Annotated[_HiddenSubSchema, Nested(_HiddenSubSchema, crop_rule=lambda _:_)]
 
 
 ID_SCHEMA = IgnoreDunderSchema("")
 
 
 def test_hidden_values():
-    assert ID_SCHEMA.dict() == {"val": "visible value"}
+    assert ID_SCHEMA.dict() == {'val': 'visible value', 'val_2': {'egg': 'spam'}, 'val_3': {'spam': 'egg'}}
 
 
 def test_get_hidden_value():
@@ -23,4 +34,5 @@ def test_get_hidden_value():
 
 
 def test_repr_output():
-    assert repr(ID_SCHEMA) == "IgnoreDunderSchema(val:str='visible value')"
+    assert repr(ID_SCHEMA) == "IgnoreDunderSchema(val:str='visible value', " \
+                              "val_2=PublicSubSchema(egg:str='spam'), val_3=_HiddenSubSchema(spam:str='egg'))"
