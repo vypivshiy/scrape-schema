629c73853342d52d05506f99ca4f88ceaa70751a
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-04-25 19:46:48 +0300

    add constructors tests

---
+++ tests/test_constructors.py
@@ -0,0 +1,36 @@
+from typing import Annotated
+
+from scrape_schema import BaseSchema
+from scrape_schema.fields.regex import ReMatch
+
+MOCK_TEXT = "lorem 100; dolor 200"
+
+
+class ConstructorSchema(BaseSchema):
+    word: Annotated[str, ReMatch(r"([a-zA-Z]+)")]
+    digit: Annotated[int, ReMatch(r"(\d+)")]
+
+
+def test_default():
+    assert ConstructorSchema(MOCK_TEXT).dict() == {"word": "lorem", "digit": 100}
+
+
+def test_from_markup():
+    assert ConstructorSchema(MOCK_TEXT).dict() == ConstructorSchema.from_markup(MOCK_TEXT).dict()
+
+
+def test_from_list():
+    lst = ConstructorSchema.from_list(MOCK_TEXT.split("; "))
+    assert [d.dict() for d in lst] == [{"word": "lorem", "digit": 100},
+                                       {"word": "dolor", "digit": 200}]
+
+
+def test_from_crop_rule():
+    assert ConstructorSchema.from_crop_rule(MOCK_TEXT, crop_rule=lambda s: s.split("; ")[-1]).dict() == \
+           {"word": "dolor", "digit": 200}
+
+
+def test_from_crop_rule_list():
+    lst = ConstructorSchema.from_crop_rule_list(MOCK_TEXT, crop_rule=lambda s: s.split("; "))
+    assert [d.dict() for d in lst] == [{"word": "lorem", "digit": 100},
+                                       {"word": "dolor", "digit": 200}]
