9178990bfd7a1171e1fd33ce3096c20d2f82d77c
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-04-28 12:18:23 +0300

    bump 0.1.0

--- scrape_schema/__init__.py
+++ scrape_schema/__init__.py
@@ -2,4 +2,4 @@ from scrape_schema import fields
 from scrape_schema.base import Annotated as ScField
 from scrape_schema.base import BaseField, BaseFieldConfig, BaseSchema, BaseSchemaConfig
 
-version = "0.0.10"
+version = "0.1.0"
