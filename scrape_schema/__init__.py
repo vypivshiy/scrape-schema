83cb2bede090d69841a1a86cb12b4de72fcbd0bb
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-04-28 18:48:46 +0300

    bump

--- scrape_schema/__init__.py
+++ scrape_schema/__init__.py
@@ -1,5 +1,11 @@
 from scrape_schema import fields
 from scrape_schema.base import Annotated as ScField
-from scrape_schema.base import BaseField, BaseFieldConfig, BaseSchema, BaseSchemaConfig
+from scrape_schema.base import (
+    BaseField,
+    BaseFieldConfig,
+    BaseSchema,
+    BaseSchemaConfig,
+    extract_fields,
+)
 
-version = "0.1.0"
+version = "0.1.1"
