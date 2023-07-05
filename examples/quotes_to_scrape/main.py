f17707c0983e477c72b7d86c22c22ec40d1044bf
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-07-05 10:03:45 +0300

    remove examples

--- examples/quotes_to_scrape/main.py
+++
@@ -1,9 +0,0 @@
-import json
-
-import requests
-from schema import QuotePage
-
-if __name__ == "__main__":
-    resp = requests.get("https://quotes.toscrape.com/page/1/").text
-    schema = QuotePage(resp)
-    print(json.dumps(schema.dict(), indent=4))
--- examples/wikipedia/main.py
+++
@@ -1,9 +0,0 @@
-import json
-
-import requests
-from schema import MainEnPage
-
-if __name__ == "__main__":
-    resp = requests.get("https://en.wikipedia.org").text
-    main_page_en = MainEnPage(resp)
-    print(json.dumps(main_page_en.dict(), indent=4))
