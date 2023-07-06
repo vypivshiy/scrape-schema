5a6ade78042d5d9502afb6c2f5b5a4b5ca0aa54c
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-07-06 22:08:09 +0300

    add __version__

--- scrape_schema/__init__.py
+++ scrape_schema/__init__.py
@@ -3,3 +3,6 @@ from scrape_schema._typing import Annotated as Sc  # pragma: no cover
 from scrape_schema.base import BaseSchema, sc_param
 from scrape_schema.field import Parsel
 from scrape_schema.nested import Nested
+
+
+__version__ = "3.0.0"
