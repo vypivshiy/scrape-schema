5bb8eb88939b5fe2d32cc5040314d8586e06ddde
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-07-16 11:41:34 +0300

    black

--- examples/cases/browser.py
+++ examples/cases/browser.py
@@ -3,9 +3,7 @@ import pprint
 from playwright.sync_api import sync_playwright
 from schema import MainPage
 
-
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
