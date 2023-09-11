90d21b465fe799380d2779b73abd8dd2d229bbb5
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-09-11 12:00:24 +0300

    refactoring

--- scrape_schema/validator.py
+++ scrape_schema/validator.py
@@ -25,7 +25,6 @@ class MarkupPreValidator:
         pattern: Optional[str] = None,
         css: Optional[str] = None,
         xpath: Optional[str] = None,
-        selector_kwargs: Optional[Dict[str, Any]] = None,
     ):
         """Pre-validation of markup for the schema.
 
@@ -35,7 +34,6 @@ class MarkupPreValidator:
             pattern: regex pattern
             css: css query
             xpath: xpath query
-            selector_kwargs: kwargs for parsel.Selector class
 
         Returns:
             Schema if all checks passed
@@ -44,7 +42,6 @@ class MarkupPreValidator:
             SchemaPreValidationError: if any match returns False
 
         """
-        self.selector_kwargs = selector_kwargs or {}
         self.xpath = xpath
         self.css = css
         self.pattern = pattern
@@ -57,12 +54,12 @@ class MarkupPreValidator:
             if self.pattern and not self._pre_validate_re(cls_self.__raw__):
                 msg = f"Failed validate re `{self.pattern}` in `{cls_self.__schema_name__}`"
                 raise SchemaPreValidationError(msg)
-            if self.css and not self._pre_validate_css(cls_self.__raw__):
+            if self.css and not self._pre_validate_css(cls_self.__selector__):
                 msg = (
                     f"Failed validate css `{self.css}` in `{cls_self.__schema_name__}`"
                 )
                 raise SchemaPreValidationError(msg)
-            if self.xpath and not self._pre_validate_xpath(cls_self.__raw__):
+            if self.xpath and not self._pre_validate_xpath(cls_self.__selector__):
                 msg = f"Failed validate xpath `{self.xpath}` in `{cls_self.__schema_name__}`"
                 raise SchemaPreValidationError(msg)
             return func(cls_self)
@@ -70,29 +67,13 @@ class MarkupPreValidator:
         return inner
 
     def _pre_validate_re(self, markup: MARKUP_TYPE) -> bool:
-        if isinstance(markup, (Selector, SelectorList)):
-            return bool(markup.re(self.pattern))  # type: ignore
-        elif isinstance(markup, bytes):
-            return bool(re.search(self.pattern, str(markup)))  # type: ignore
         return bool(re.search(self.pattern, markup))  # type: ignore
 
     def _pre_validate_css(self, markup: MARKUP_TYPE) -> bool:
-        if isinstance(markup, (Selector, SelectorList)):
-            return bool(markup.css(self.css).get())  # type: ignore
-        elif isinstance(markup, bytes):
-            return bool(  # type: ignore
-                Selector(body=markup, **self.selector_kwargs).css(self.css).get()  # type: ignore
-            )
-        return bool(Selector(markup, **self.selector_kwargs).css(self.css).get())  # type: ignore
+        return bool(markup.css(self.css).get())  # type: ignore
 
     def _pre_validate_xpath(self, markup: MARKUP_TYPE) -> bool:
-        if isinstance(markup, (Selector, SelectorList)):
-            return bool(markup.xpath(self.xpath).get())  # type: ignore
-        elif isinstance(markup, bytes):
-            return bool(
-                Selector(body=markup, **self.selector_kwargs).xpath(self.xpath).get()  # type: ignore
-            )
-        return bool(Selector(markup, **self.selector_kwargs).xpath(self.xpath).get())  # type: ignore
+        return bool(markup.xpath(self.xpath).get())  # type: ignore
 
 
 markup_pre_validator = MarkupPreValidator
