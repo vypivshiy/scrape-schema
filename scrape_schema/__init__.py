8bc12cb8f3c610cd1f0952a546bdbc6fbfb7b248
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-09-21 16:56:24 +0300

    bump

--- scrape_schema/__init__.py
+++ scrape_schema/__init__.py
@@ -1,7 +1,7 @@
 # Annotated shortcut alias
 from scrape_schema._typing import Annotated as Sc  # pragma: no cover
 from scrape_schema.base import BaseSchema, sc_param
-from scrape_schema.field import JMESPath, Parsel, Text
+from scrape_schema.field import Callback, JMESPath, Parsel, Text
 from scrape_schema.nested import Nested
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
