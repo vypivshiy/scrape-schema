ecdfb7597c729ee293f27b6c56b23eb2d873ef27
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-10-05 18:08:55 +0300

    add raise exception test

--- tests/test_raw_dl.py
+++ tests/test_raw_dl.py
@@ -1,4 +1,6 @@
-from scrape_schema.field import DLRawField
+import pytest
+
+from scrape_schema.field import RawDLField
 
 # https://www.w3resource.com/html/definition-lists/HTML-definition-lists-dl-dt-dd-tags-elements.php
 DL_HTML = """
@@ -13,31 +15,35 @@ DL_HTML = """
 
 
 def test_dl_raw_parse():
-    assert DLRawField().css_dl().sc_parse(DL_HTML) == {
+    assert RawDLField().css_dl().sc_parse(DL_HTML) == {
         "PHP": [" A server side scripting language. "],
         "JavaScript": [" A client side scripting language. "],
     }
 
 
 def test_dl_raw_arguments():
-    assert DLRawField().css_dl(strip=True).sc_parse(DL_HTML) == {
+    assert RawDLField().css_dl(strip=True).sc_parse(DL_HTML) == {
         "PHP": ["A server side scripting language."],
         "JavaScript": ["A client side scripting language."],
     }
 
-    assert DLRawField().css_dl(strip=True, str_join="").sc_parse(DL_HTML) == {
+    assert RawDLField().css_dl(strip=True, str_join="").sc_parse(DL_HTML) == {
         "JavaScript": "A client side scripting language.",
         "PHP": "A server side scripting language.",
     }
 
-    assert DLRawField().css_dl(
+    assert RawDLField().css_dl(
         strip=True, re_sub_pattern=r"A|a", re_sub_count=1
     ).sc_parse(DL_HTML) == {
         "JvaScript": [" client side scripting lnguge."],
         "PHP": [" server side scripting lnguge."],
     }
-    assert DLRawField().css_dl(strip=True, re_sub_pattern=r"A|a").sc_parse(DL_HTML) == {
+    assert RawDLField().css_dl(strip=True, re_sub_pattern=r"A|a").sc_parse(DL_HTML) == {
         "JvScript": [" client side scripting lnguge."],
         "PHP": [" server side scripting lnguge."],
     }
-    print()
+
+
+def test_dl_row_fail():
+    with pytest.raises(TypeError):
+        RawDLField().css_dl().sc_parse("")
